0ea51e416a53c41e33a1130e987d1f4ed63afe90
Alberto Bueno <abettetto@gmail.com>
2022-05-24 22:49:47 -0500

    Make a histogram, usign some propierties relate with histograms

---
+++ plotting_app/pyplot.py
@@ -0,0 +1,29 @@
+'''
+This app simulates 1000 coins flips and stores those 
+values in a list we call binom_dist. 
+When the sample (with replacement) 100 from that list, 
+take the mean, and stores thatr mean inthe cleverly named list_of_means.
+'''
+
+import streamlit as st 
+import numpy as np
+import matplotlib.pyplot as plt 
+
+perc_heads = st.number_input (label='Chance of coins landing on Heads', min_value = 0.0, max_value = 1.0, value = .25)
+graph_title = st.text_input (label='Graph Title')
+binom_dist = np.random.binomial(1, perc_heads ,1000)
+
+list_of_means =[]
+for i in range (0,1000):
+    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
+
+fig, ax = plt.subplots()
+ax = plt.hist(list_of_means)
+plt.title(graph_title)
+st.pyplot(fig)
+
+#fig2, ax2 = plt.subplots()
+#ax2 = plt.hist([1,1,1,1])
+#st.pyplot(fig2)
+
+
