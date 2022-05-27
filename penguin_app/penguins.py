1a5e771700db8ebc501e01d117de0b8671d2ca0a
Alberto Bueno <abettetto@gmail.com>
2022-05-26 22:54:21 -0500

    Se agregan controles de entrada del grafico

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -1,7 +1,27 @@
+from select import select
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
 st.title ("Palmer's penguin")
+st.markdown('Use  this Streamlit app to make your own scatterplot abou penguins!')
+
+selected_species = st.selectbox('What species would you like to visualize?', ['Adelie', 'Gentoo', 'Chinstrap'])
+selected_x_var = st.selectbox('What do want the x variable to be?',['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 
+'body_mass_g'] )
+selected_y_var = st.selectbox('What about the y?',['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 
+'body_mass_g'] )
 
 penguins_df = pd.read_csv('penguins.csv')
-st.write(penguins_df.head())
+# st.write(penguins_df.head())
+
+sns.set_style('darkgrid')
+markers = {'Adelie': "X", "Gentoo":"s", "Chinstrap":'o'}
+
+fig, ax = plt.subplots()
+ax = sns.scatterplot(x = penguins_df[selected_x_var], y = penguins_df[selected_y_var], hue = penguins_df['species'], markers = markers)
+plt.xlabel(selected_x_var)
+plt.xlabel(selected_y_var)
+plt.title('Scatterplot () Penguins'.format(selected_species))
+st.pyplot(fig)
