1409730943f249e2e5b59318009a47ee89d0728a
Alberto Bueno <abettetto@gmail.com>
2022-06-04 23:07:01 -0500

    Add files to make graphs using bokeh and altair into streamlit

---
+++ trees_app/bokeh..py
@@ -0,0 +1,20 @@
+import streamlit as st 
+import pandas as pd 
+from bokeh.plotting import figure
+
+st.title('SF trees')
+st.write('This app analyses trees in San Francisco using'
+         ' a dataset kindly provided by SF DPW')
+st.subheader('Bokeh Chart')
+trees_df = pd.read_csv('trees.csv')
+
+scatterplot = figure (title = 'Bokeh Scatterplot')
+scatterplot.scatter(trees_df['dbh'], trees_df['site_order'])
+st.bokeh_chart(scatterplot)
+scatterplot.xaxis.axis_label = "dbh"
+
+scatterplot = figure(title = 'Bokeh Scatterplot')
+scatterplot.scatter(trees_df['dbh'], trees_df['site_order'])
+scatterplot.yaxis.axis_label = "site_order"
+scatterplot.xaxis.axis_label = "dbh"
+st.bokeh_chart(scatterplot)
